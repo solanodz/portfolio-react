9da5fae02dc47963d7a1ce22f49a2e436f1c969a
solano de zuasnabar <sdezuasnabar@gmail.com>
2023-09-26 20:59:47 -0300

    link de downloads correjidos

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,10 +1,10 @@
 import dibujo from "../assets/Hand coding-amico.svg"
 import Reveal from "./Reveal"
-import desarrolloWeb from '/public/downloads/Desarrollo_Web_Coderhouse.png'
-import ingles from '/public/downloads/EF SET Certificate.pdf'
-import javascript from '/public/downloads/javascript.png'
-import react from '/public/downloads/react.png'
-import frontEnd from '/public/downloads/front-end-certificado.png'
+import desarrolloWeb from '/'
+import ingles from '/EF SET Certificate.pdf'
+import javascript from '/javascript.png'
+import react from '/react.png'
+import frontEnd from '/front-end-certificado.png'
 
 const SobreMi = () => {
     return (
@@ -21,11 +21,11 @@ const SobreMi = () => {
                 <div className="flex flex-col justify-around mt-12 sm:flex-row">
                     <Reveal>
                         <div className='flex flex-col items-start my-4  md:1/2'>
-                            <a href="/public/downloads/EF SET Certificate.pdf" download={ingles} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200'>Nivel de Inglés - EFSET</a>
-                            <a href="/public/downloads/Desarrollo_Web_Coderhouse.png" download={desarrolloWeb} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200'>Desarrollo Web - Coderhouse</a>
-                            <a href="/public/downloads/javascript.png" target="_blank" download={javascript} className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200'>JavaScript - Coderhouse</a>
-                            <a href="/public/downloads/react.png" target="_blank" download={react} className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200'>React - Coderhouse</a>
-                            <a href="/public/downloads/front-end-certificado.png" download={frontEnd} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200'>Desarrollador Front-end - Coderhouse</a>
+                            <a href={ingles} download={ingles} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Nivel de Inglés - EFSET</a>
+                            <a href={desarrolloWeb} download={desarrolloWeb} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Desarrollo Web - Coderhouse</a>
+                            <a href={javascript} target="_blank" download={javascript} className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">JavaScript - Coderhouse</a>
+                            <a href={react} target="_blank" download={react} className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">React - Coderhouse</a>
+                            <a href={frontEnd} download={frontEnd} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Desarrollador Front-end - Coderhouse</a>
                         </div>
                     </Reveal>
                     <Reveal>
