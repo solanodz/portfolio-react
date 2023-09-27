14ee7cf00c8cfeb83cb519d31fce94fbdeffcb42
solano de zuasnabar <sdezuasnabar@gmail.com>
2023-09-26 21:17:21 -0300

    linkeo a certificados

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,7 +1,7 @@
 import dibujo from "../assets/Hand coding-amico.svg"
 import Reveal from "./Reveal"
-import desarrolloWeb from '/'
-import ingles from '/EF SET Certificate.pdf'
+import desarrolloWeb from '/public/Desarrollo_Web_Coderhouse.png'
+import ingles from '/EFSET-certificate.pdf'
 import javascript from '/javascript.png'
 import react from '/react.png'
 import frontEnd from '/front-end-certificado.png'
@@ -21,11 +21,11 @@ const SobreMi = () => {
                 <div className="flex flex-col justify-around mt-12 sm:flex-row">
                     <Reveal>
                         <div className='flex flex-col items-start my-4  md:1/2'>
-                            <a href={ingles} download={ingles} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Nivel de Inglés - EFSET</a>
-                            <a href={desarrolloWeb} download={desarrolloWeb} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Desarrollo Web - Coderhouse</a>
-                            <a href={javascript} target="_blank" download={javascript} className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">JavaScript - Coderhouse</a>
-                            <a href={react} target="_blank" download={react} className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">React - Coderhouse</a>
-                            <a href={frontEnd} download={frontEnd} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Desarrollador Front-end - Coderhouse</a>
+                            <a href={ingles} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Nivel de Inglés - EFSET</a>
+                            <a href={desarrolloWeb} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Desarrollo Web - Coderhouse</a>
+                            <a href={javascript} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">JavaScript - Coderhouse</a>
+                            <a href={react} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">React - Coderhouse</a>
+                            <a href={frontEnd} target="_blank" className='bg-accent text-white text-center border-2 border-accent py-2 px-7 text-sm font-medium my-1 rounded-md sm:py-3 w-full hover:bg-black hover:text-accent duration-200' rel="noreferrer">Desarrollador Front-end - Coderhouse</a>
                         </div>
                     </Reveal>
                     <Reveal>
